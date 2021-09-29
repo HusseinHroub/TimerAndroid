e5d0abd2bc6d0c549d9a8b6593b9b206572ed092
Hussein <husseinhroub311@gmail.com>
2021-09-29 13:09:10 +0300

    Initial commit

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +13,10 @@ import com.example.timer.viewerImpl.ViewerImp;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String START_TIMER = "Start Timer";
+    private static final String STOP_TIMER = "Stop Timer";
+    private static final int STOP_STATUS = 0;
+    private static final int START_STATUS = 1;
     private TimerProcessor timerProcessor;
 
     @Override
@@ -20,20 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timerProcessor = new TimerProcessor(new ViewerImp(this), new Handler(Looper.getMainLooper()));
+        timerProcessor.setOnFinishedRunnable(this::onFinishedTime);
+        timerProcessor.setOnStoppedRunnable(this::onStoppedTime);
+        Button button = findViewById(R.id.start_timer_button);
+        button.setTag(STOP_STATUS);
 
     }
 
     public void startTimer(View view) {
-        view.setEnabled(false);
-        timerProcessor.startTimerCountDownProcess(this::timerProcessFinishedCalLBack);
+        if ((int) view.getTag() == START_STATUS) {
+            timerProcessor.interrupt();
+            view.setTag(STOP_STATUS);
+        } else {
+            Button button = (Button) view;
+            button.setText(STOP_TIMER);
+            timerProcessor.startTimerCountDownProcess();
+            view.setTag(START_STATUS);
+        }
+
     }
 
 
-    private void timerProcessFinishedCalLBack() {
-        findViewById(R.id.start_timer_button).setEnabled(true);
+    private void onFinishedTime() {
+        Button button = findViewById(R.id.start_timer_button);
+        button.setText(START_TIMER);
         MediaPlayer mp = MediaPlayer.create(this, R.raw.ending_sound);
         mp.setOnCompletionListener(MediaPlayer::release);
         mp.start();
 
+    }
+
+    private void onStoppedTime() {
+        Button button = findViewById(R.id.start_timer_button);
+        button.setText(START_TIMER);
     }
 }
--- app/src/main/java/com/example/timer/TimerProcessor.java
+++ app/src/main/java/com/example/timer/TimerProcessor.java
@@ -12,30 +12,65 @@ public class TimerProcessor {
     private long startTimeInNano;
     private long remainingTimeInNano;
     private Viewer viewer;
+    private boolean interrupted;
+    private boolean onProgress;
+    private Runnable onFinishedRunnable;
+    private Runnable onStoppedRunnable;
 
     public TimerProcessor(Viewer viewer, Handler handler) {
         this.viewer = viewer;
         this.handler = handler;
     }
 
-    public void startTimerCountDownProcess(Runnable timerFinishRunnable) {
+    public void setOnFinishedRunnable(Runnable onFinishedRunnable) {
+        this.onFinishedRunnable = onFinishedRunnable;
+    }
+
+    public void setOnStoppedRunnable(Runnable onStoppedRunnable) {
+        this.onStoppedRunnable = onStoppedRunnable;
+    }
+
+    public void startTimerCountDownProcess() {
+        if (onProgress) {
+            return;
+        }
+        onProgress = true;
+        interrupted = false;
         remainingTimeInNano = viewer.getInitialTimerValueINNano();
         startTimeInNano = System.nanoTime();
         handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             public void run() {
+                if (interrupted) {
+                    if (onStoppedRunnable != null) {
+                        onStoppedRunnable.run();
+                    }
+                    onProgress = false;
+                    return;
+                }
                 long elapsedTimeInNanoSinceStart = System.nanoTime() - startTimeInNano;
                 remainingTimeInNano -= elapsedTimeInNanoSinceStart;
                 viewer.setTimeElapsedValue(remainingTimeInNano);
                 if (remainingTimeInNano < 0) {
                     viewer.setTimeElapsedValue(0);
                     handler.removeCallbacks(this);
-                    timerFinishRunnable.run();
+                    if (onFinishedRunnable != null) {
+                        onFinishedRunnable.run();
+                    }
+                    onProgress = false;
                     return;
                 }
                 startTimeInNano = System.nanoTime();
                 handler.postDelayed(this, DELAY_IN_MILLIS);
             }
         });
+    }
+
+    public void interrupt() {
+        interrupted = true;
+    }
+
+    public boolean isInterrupted() {
+        return interrupted;
     }
 }
